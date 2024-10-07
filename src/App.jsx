cc5f11d21639f1b89dd608750af4f27f66b0ff86
pulemashile <166323606+pulemashile@users.noreply.github.com>
2024-10-07 09:32:46 +0200

    fixing styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import "./App.css";
 import Navbar from "./home/navbar";
 import { Container, Grid, Typography } from "@mui/material";
-import imageone from  './assets/images/image-hero-desktop.png'
+import imageone from './assets/images/image-hero-desktop.png';
 import Button from '@mui/material/Button';
-
 
 function App() {
   return (
@@ -16,79 +15,60 @@ function App() {
           marginTop: "10vh",
         }}
       >
-        <Grid container spacing={5}>
+        <Grid container spacing={7}> {/* Increased spacing */}
           <Grid item lg={7}>
-          <Typography
-          variant="h2"
-          style={{
-            fontWeight: "bolder",
-          }}
-        >
-          make{" "}
-        </Typography>
-        <Typography
-          variant="h2"
-          style={{
-            fontWeight: "bolder",
-          }}
-        >
-          remote work{" "}
-        </Typography>
-        <div>
-          <Typography
-            variant="h6"
-            style={{ fontSize: "18px", marginBlock: "40px", width : '30vw' }}
-          >
-            get your team in sync ,no matter your location. streamline processes,create team rituals and  watch productivity roar
-
-            
-          </Typography>
-          <Button 
-          type="submit" 
-          variant="outlined" 
-          sx={{
-            color: 'white',
-            borderColor: 'black',
-            backgroundColor: 'black',
-            borderRadius:"12px",
-            
-            '&:hover': {
-              backgroundColor: 'white',
-              color: 'black',
-              
-
-            
-  
-            },
-          }}
-        > 
-          Learn more
-        </Button>
-        </div >
-        
+            <Typography variant="h2" style={{ fontWeight: "bolder" }}>
+              make{" "}
+            </Typography>
+            <Typography variant="h2" style={{ fontWeight: "bolder" }}>
+              remote work{" "}
+            </Typography>
+            <div>
+              <Typography
+                variant="h6"
+                style={{ fontSize: "18px", marginBlock: "40px", width: '30vw' }}
+              >
+                get your team in sync, no matter your location. streamline processes, create team rituals and watch productivity roar
+              </Typography>
+              <Button 
+                type="submit" 
+                variant="outlined" 
+                sx={{
+                  color: 'white',
+                  borderColor: 'black',
+                  backgroundColor: 'black',
+                  borderRadius: "12px",
+                  transition: 'background-color 0.3s, color 0.3s', // Added transition for smoother effect
+                  '&:hover': {
+                    backgroundColor: 'white',
+                    color: 'black',
+                  },
+                }}
+              > 
+                Learn more
+              </Button>
+            </div>
           </Grid>
           <Grid item lg={5}>
             <div>
-               <img  src={imageone} style={{
-                height : '70vh',
-                objectFit : 'cover',
-               }}/>
+              <img 
+                src={imageone} 
+                style={{
+                  height: '70vh',
+                  objectFit: 'cover',
+                }} 
+              />
             </div>
           </Grid>
-          
-        </Grid >
+        </Grid>
         <div className="container">
-      <div><img src="./src\assets\images\client-databiz.svg" /></div>
-      <div><img src="./src\assets\images\client-audiophile.svg" /></div>
-      <div><img src="src\assets\images\client-meet.svg "/></div>
-      <div><img src="src\assets\images\client-maker.svg"/></div>
-          
+          <div><img src="./src/assets/images/client-databiz.svg" alt="Client Databiz" /></div>
+          <div><img src="./src/assets/images/client-audiophile.svg" alt="Client Audiophile" /></div>
+          <div><img src="./src/assets/images/client-meet.svg" alt="Client Meet" /></div>
+          <div><img src="./src/assets/images/client-maker.svg" alt="Client Maker" /></div>
         </div>
-        
       </Container>
-      
     </div>
-  
   );
 }
 
